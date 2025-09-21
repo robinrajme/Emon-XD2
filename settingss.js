deb829e51e80b123bac2bab182b00e4447718224
robinrajme <robinraj.me@gmail.com>
2025-09-22 02:09:45 +0800

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~AzFiXATD#atC5URqQEDws9zNeWmL-NHi1R8ZYZI3JW9vmwFGVScE",  // add your Session Id here
   OWNER_NUMBER: "8801309991724", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Asia/Dhaka" //put your country timeZone....leave blank if u don't know.
